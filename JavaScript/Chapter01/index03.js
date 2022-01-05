68435c80c213e9a3e81a50efda5f87ab8a17921b
이정현 <5337myung@gmail.com>
2022-01-05 23:22:40 +0900

    JavaScript - 조건문 논리연산자

---
+++ JavaScript/Chapter01/index03.js
@@ -0,0 +1,45 @@
+// 표현식 && 표현식
+
+if (true && true) {
+    console.log('두개 모두 참이면 참이다');
+}
+
+if (false && false) {
+    console.log('두개 모두 거짓이면 거짓');
+}
+
+
+if (true && false) {
+    console.log('한개만 참이면 거짓');
+}
+
+if (false && true) {
+    console.log('한개만 참이면 거짓');
+}
+
+
+// 표현식 || 표현식
+if (true || true) {
+    console.log('두개 모두 참이면 참이다');
+}
+
+if (true || false) {
+    console.log('한개만 참이면 참');
+}
+
+if (false || true) {
+    console.log('한개만 참이면 거짓');
+}
+
+if (false || false) {
+    console.log('한개만 참이면 거짓');
+}
+
+// !표현식
+if(!true) {
+    console.log('참이면 거짓');
+}
+
+if(!false) {
+    console.log('거짓이면 참');
+}
