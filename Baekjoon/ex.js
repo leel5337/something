2de0770166f892b6625e34bd928170d8160bab44
이정현 <5337myung@gmail.com>
2022-01-06 06:41:38 +0900

    JavaScript - 백준알고리즘 연습장1

---
+++ Baekjoon/ex.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('./.idea/input.txt').toString().split(' ');
+
+const num1 = Number(input[0]);
+const num2 = Number(input[1]);
+
+console.log(num1 + num2);
