d9c507d65f53916d80d97e18c544845e237adb25
이정현 <5337myung@gmail.com>
2022-01-17 11:23:34 +0900

    JavaScript - 반복문 for

---
+++ JavaScript/Chapter02/index02.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < 10; i--) {
+    console.log(i);
+}
+
+for(let j = 10; j >= 0; j-=2) {
+    console.log(j);
+}
+
+// for문과 배열을 같이 사용
+const names = ['강아지', '고양이', '호랑이'];
+
+for(let i = 0; i < names.length; i++) {
+    console.log(name[i]);
+}
