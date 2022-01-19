ac91ffb7ec7a499305ce6acf2ceafe8b9b119596
이정현 <5337myung@gmail.com>
2022-01-19 23:42:26 +0900

    JavaScript - 배열함수 map

---
+++ JavaScript/Chapter02/index06.js
@@ -0,0 +1,18 @@
+const array = [1, 2, 3, 4, 5];
+const squared = array.map(n => n + n);
+
+console.log(squared);
+
+
+const items = [
+    {
+        id: 1,
+        text: 'good'
+    },
+    {
+        id: 2,
+        text: 'bad'
+    }
+];
+const texts = items.map(item => item.text);
+console.log(texts);
