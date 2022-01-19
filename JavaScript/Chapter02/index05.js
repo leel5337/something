54c222dd6bc38d0f8b4bbb8e651b6055366d2174
이정현 <5337myung@gmail.com>
2022-01-19 23:05:35 +0900

    JavaScript - 배열함수 forEach

---
+++ JavaScript/Chapter02/index05.js
@@ -0,0 +1,9 @@
+const soccerplayer = ['차범근', '박지성', '손흥민', '기성용'];
+
+for (let i = 0; i < soccerplayer.length; i++) {
+    console.log(soccerplayer[i]);
+}
+
+soccerplayer.forEach(player => {
+    console.log(player);
+})
